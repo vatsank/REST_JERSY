1e10dac6218ba3568d63d1466a188c12cf49c736
Srivatsan <vatsank@gmail.com>
2018-06-08 17:16:44 +0530

    Updated Cors Filter

--- Rest_Basic_Auth/src/com/training/execeptions/CorsFilter.java
+++ Rest_Basic_Auth/src/com/training/execeptions/CorsFilter.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
 
+@Provider
 public class CorsFilter implements ContainerResponseFilter {
 
 	@Override
